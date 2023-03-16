417f4f390d4811cfab7029e27e7e0b52b884a821
Andre <sarreqiandrea@gmail.com>
2023-03-16 18:47:02 +0000

    Adding the nextQuestion function

--- assets/js/script.js
+++ assets/js/script.js
@@ -68,3 +68,7 @@ document.addEventListener('DOMContentLoaded', pageLoaded);
 function pageLoaded() {
     document.getElementById("question-number").innerHTML = currentQuestionIndex;
 }
+
+function nextQuestion () {
+
+}
